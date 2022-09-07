16b33d7cbe8d4a6fce262dc5402a1f3af23a75bb
Zahra-imdad <zahra.imdadali@gmail.com>
2022-09-08 03:21:43 +0500

    "fourth"

--- src/app/pages/edit-blog/edit-blog.component.ts
+++ src/app/pages/edit-blog/edit-blog.component.ts
@@ -9,108 +9,97 @@ import { BlogService } from 'src/app/services/blog.service';
   styleUrls: ['./edit-blog.component.css']
 })
 export class EditBlogComponent implements OnInit {
-  blogs:any[]=[]
-  showDiv:boolean =false 
-  currentId:any=''
-  editTitle: any;
-  editVal:string=''
-  contentVal:string=''
-  tagVal:any;
-  updateId:string=''
-  actionStatus:string='default'
-  totalBlogs:number=0
-  tagsCount:number=0
+  blogs: any[] = []
+  showDiv: boolean = false
+  currentId: any = ''
+  editVal: string = ''
+  contentVal: string = ''
+  updateId: string = ''
+  actionStatus: string = 'default'
+  totalBlogs: number = 0
 
-  tag:String[]=[];
-  //tagStore:any[]=[]
+  tag: String[] = [];
 
-  @ViewChild('cardTitle') cardTitle:any
-  @ViewChild('cardContent') cardContent:any
-  @ViewChild('inputTitle') inputTitle:any
-  @ViewChild('inputContent') inputContent:any
-  @ViewChild('cardTags') cardTags:any
 
-  fg:any = new FormControl()
+  @ViewChild('cardTitle') cardTitle: any
+  @ViewChild('cardContent') cardContent: any
+  @ViewChild('inputTitle') inputTitle: any
+
+
+  fg: any = new FormControl()
   fb = new FormBuilder()
 
-  constructor(private blogService:BlogService,private authServie:AuthService) { }
+  constructor(private blogService: BlogService, private authServie: AuthService) { }
 
   ngOnInit(): void {
     this.fg = this.fb.group({
-      title: ['',[Validators.required]],
-      content: ['',[Validators.required,Validators.minLength(550)]],
-      
+      title: ['', [Validators.required]],
+      content: ['', [Validators.required, Validators.minLength(550)]],
+
     })
     this.currentId = this.authServie.getLocalStorage();
     this.getBlogs()
-    
+
   }
-  getBlogs(){
-    this.blogService.CurrentUserBlogs(this.currentId).subscribe((data:any)=>{
+  getBlogs() {
+    this.blogService.CurrentUserBlogs(this.currentId).subscribe((data: any) => {
       console.log(data.blogPosts)
       this.blogs = data.blogPosts
       this.totalBlogs = this.blogs.length
-      console.log("TAGS",this.blogs)
+      console.log("TAGS", this.blogs)
     })
   }
-  edit(blog:any,e:any){
-    this.showDiv=true
+  edit(blog: any, e: any) {
+    this.showDiv = true
     window.scrollTo(0, 0)
-    console.log(this.cardTitle)
-    this.editVal =this.cardTitle.nativeElement.value
+
+    this.editVal = this.cardTitle.nativeElement.value
     this.editVal = blog.title
+
     this.contentVal = this.cardContent.nativeElement.innerHTML
     this.contentVal = blog.content
-    // this.tagVal = this.cardTags.nativeElement.innerHTML
-    // this.tagVal = blog.tags
 
     this.tag = blog.tags
-    // console.log("TagsVal",this.tagVal)
-    // this.tag = this.tagVal
-    console.log("Tags Val",this.tag)
     this.updateId = blog._id;
-    
-  }
-  updateBlog(e:any){
-    this.actionStatus = "UPDATED SUCCESSFULLY"
-    this.editVal=this.inputTitle.nativeElement.value
 
-   
+  }
+  updateBlog(e: any) {
+    this.actionStatus = "UPDATED SUCCESSFULLY"
+    this.editVal = this.inputTitle.nativeElement.value
+
+
     console.log(this.tag)
-    this.blogService.updateBlog(this.updateId,{title:this.editVal,content:this.contentVal,tags:this.tag}).subscribe((data)=>{
+    this.blogService.updateBlog(this.updateId, { title: this.editVal, content: this.contentVal, tags: this.tag }).subscribe((data) => {
       console.log(data)
       this.getBlogs()
-      
+
     })
-    this.showDiv = false 
+    this.showDiv = false
   }
-  delete(blog:any,e:any){
+  delete(blog: any, e: any) {
     console.log(blog._id)
-    this.blogService.deleteBlog(blog._id).subscribe((data)=>{
-      this.actionStatus=`DELELTED BLOG : ${blog._id}`
+    this.blogService.deleteBlog(blog._id).subscribe((data) => {
+      this.actionStatus = `DELELTED BLOG : ${blog._id}`
       console.log(data)
       this.getBlogs()
     })
-    
+
   }
-  get TitleInput(){
+  get TitleInput() {
     return this.fg.get('title')
   }
-  get ContentInput(){
+  get ContentInput() {
     return this.fg.get('content')
   }
 
-  onTagAdd(e:any){
-    //console.log("ADD TAG: ",this.tag)
-    
-    //this.tag.push(e.value)
-    // console.log("VALUE EVENT: ",e.value)
-    // console.log("on Tag ADD:" ,this.tag)
+  onTagAdd(e: any) {
+    console.log("ADD TAG: ", this.tag)
+    console.log("on Tag ADD:", this.tag)
   }
-  onTagRemove(e:any){
-    this.tag = this.tag.filter( (tag) => tag !== e)
-    console.log("EVENT : ",e)
-    console.log("TAG : ",this.tag)
+  onTagRemove(e: any) {
+    this.tag = this.tag.filter((tag) => tag !== e)
+    console.log("EVENT : ", e)
+    console.log("TAG : ", this.tag)
   }
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,71 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
- username:string = ''
- email:string= ''
-  password:string=''
-  registerError=false
-  fg:any = new FormControl()
-  fb = new FormBuilder()
-  nameInput:any = null
-  emailInput:any = null
-  passwordInput:any = null
-  userRegistered:boolean=false
-  Loading:boolean=false;
+  registerError = false;
+  fg: any = new FormControl();
+  fb = new FormBuilder();
+  clicked: boolean = false;
+  userRegistered: boolean = false;
+  Loading: boolean = false;
 
-  constructor(private authService:AuthService,private router:Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.fg = this.fb.group({
-      username: ['',[Validators.required,Validators.minLength(4)]],
-      email: ['',[Validators.required,Validators.email]],
-      password:['',Validators.required]
-    })
-    // this.fg = new FormGroup({
-    //   username :new FormControl('',[Validators.required,Validators.minLength(4)]),
-    //   email:new FormControl('',[Validators.required,Validators.email])
-    // })
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
- 
-  register(){
-    if(this.fg.valid){
-      this.Loading=true
-      this.fg.valueChanges.subscribe((data:any)=>{console.log(data)})
-      console.log("registered")
-      this.authService.register(this.fg.value.username,this.fg.value.email,this.fg.value.password).subscribe((data)=>{
-        console.log(data)
-        this.router.navigate(['/login'])
-      },err=>{
-        this.userRegistered=true
-        console.log(err.message)
-      },()=>{
-        this.Loading=false;
-      })
-    }else{
-      this.registerError=true
-    }
-    // this.nameInput = new FormControl('',[Validators.required,Validators.minLength(4)])
-    // this.emailInput = new FormControl('',[Validators.required,Validators.email])
-    // this.passwordInput = new FormControl('',[Validators.required,Validators.minLength(4)])
-    console.log(this.fg.valid)
-    
+  get UsernameInput() {
+    return this.fg.get('username');
+  }
+  get EmailInput() {
+    return this.fg.get('email');
+  }
+  get PasswordInput() {
+    return this.fg.get('password');
+  }
 
-  }
-  get UsernameInput(){
-    return this.fg.get('username')
-  }
-  get EmailInput(){
-    return this.fg.get('email')
-  }
-  get PasswordInput(){
-    return this.fg.get('password')
+  register() {
+    this.clicked = true;
+    if (this.fg.valid) {
+      this.Loading = true;
+      this.fg.valueChanges.subscribe((data: any) => {
+        console.log(data);
+      });
+      this.authService
+        .register(
+          this.fg.value.username,
+          this.fg.value.email,
+          this.fg.value.password
+        )
+        .subscribe(
+          (data) => {
+            this.router.navigate(['/login']);
+          },
+          (err) => {
+            this.userRegistered = true;
+            console.log(err.message);
+          },
+          () => {
+            this.Loading = false;
+          }
+        );
+    } else {
+      this.registerError = true;
+    }
   }
 }
